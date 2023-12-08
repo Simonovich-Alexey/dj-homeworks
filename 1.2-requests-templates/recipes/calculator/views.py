e2fc15a45421ab976dec9994adfba281ab6f8c05
Alexey_Sim <simonovich_alexey@bk.ru>
2023-12-08 13:31:31 +0500

    Changes views.py, urls.py

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -15,16 +15,13 @@ DATA = {
         'колбаса, ломтик': 1,
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
-    },
-    # можете добавить свои рецепты ;)
+    }
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+
+def recipes(request, name):
+    number = int(request.GET.get('servings', 1))
+    context = {
+        'recipe': {ingredient:  amount * number for ingredient, amount in DATA.get(name).items()}
+    }
+    return render(request, 'calculator/index.html', context)
